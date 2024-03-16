e9ce05fe7b1bd2e53c6b6b7f423c5eb7c8e4a9c6
Siratee Topradit <tae.siratee@gmail.com>
2024-03-16 20:07:49 +0700

    Implemented player movement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
 
-public class Main extends{
+public class Main{
     public static void main(String[] args) {
+        JFrame window = new JFrame("Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GamePanal gamePanal = new GamePanal();
+        window.add(gamePanal);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanal.startGameThread();
+
     }
 }
