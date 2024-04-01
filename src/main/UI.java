90907f7b7762f88fdb1578da74bf04f1b30bc3d4
Siratee Topradit <tae.siratee@gmail.com>
2024-04-01 11:00:48 +0700

    Add UI drawing functionality

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,2 +1,20 @@
-package main;public class UI {
+package main;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class UI {
+    GamePanel gp;
+    Font Courier_New_40 = new Font("Courier New", 40);
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+    }
+    public void draw(GraphicsContext gc) {
+        gc.setFont(Courier_New_40);
+        gc.setFill(Color.WHITE);
+        gc.fillText("Score:", 10 , 40);
+    }
+
 }
