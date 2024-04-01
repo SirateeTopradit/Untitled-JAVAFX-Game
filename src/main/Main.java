de861cff2676ba1a3e3afac3c549ba09025af608
Siratee Topradit <tae.siratee@gmail.com>
2024-04-01 11:00:39 +0700

    Update window title in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Set the title of the window
-        primaryStage.setTitle("Game");
+        primaryStage.setTitle("Untitled-JAVAFX-Game");
 
         // Override the default close request action
         primaryStage.setOnCloseRequest(event -> {
