f63f81aaa975b8be2835c84beaedcc2334a74749
Siratee Topradit <tae.siratee@gmail.com>
2024-04-08 20:26:19 +0700

    Revert "Fix translation bug in MapManager.java"
    This reverts commit 0ff7e092308ec15f6bd5f8a8d48f580f38299694.

--- src/map/MapManager.java
+++ src/map/MapManager.java
@@ -29,7 +29,8 @@ public class MapManager {
 
     public void drawMap(GraphicsContext g2d) {
         double translateX = (double) gp.getScreenWidth() / 2 - player.getWorldX();
-        double translateY = (double) gp.getScreenHeight() / 2 - player.getWorldY()-10;
+//        System.out.println(player.getWorldX());
+        double translateY = (double) gp.getScreenHeight() / 2 - player.getWorldY();
         g2d.drawImage(image, translateX, translateY, gp.getWorldScreenWidth(), gp.getWorldScreenHeight());
     }
 }
