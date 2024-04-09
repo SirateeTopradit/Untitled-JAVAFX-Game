8853b22e40c2e127d96a3be7d9b181dfa0baa9dc
Siratee Topradit <tae.siratee@gmail.com>
2024-04-09 16:16:37 +0700

    Add Zomby class with walking animation

--- src/entity/Zomby.java
+++ src/entity/Zomby.java
@@ -1,2 +1,32 @@
-package entity;public class Zomby {
+package entity;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import main.GamePanel;
+
+import java.io.InputStream;
+
+public class Zomby extends Entity{
+    public Zomby(GamePanel gp) {
+        super(gp);
+        setSpeed(5);
+
+        setHitBox(new Rectangle(4*20, 5*20, 2*20, 4*20));
+        setHitBoxWalk(new Rectangle(3*20, 5*20, 4*20, 4*20));
+        try {
+            for (int j = 0; j < 1; j++) {
+                int direction = j;
+                for (int i = 0; i < getNUM_FRAMES(); i++) {
+                    InputStream resource = getClass().getClassLoader().getResourceAsStream(String.format("Zomby/Zomby_walking%d_/Zomby_walking%d_%02d.png", direction, direction, i));
+                    if (resource != null) {
+                        getFrames()[j][i] = new Image(resource);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
