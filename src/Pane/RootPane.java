cbee2a45465d269f672936767cba22c46a08ba5d
Siratee Topradit <tae.siratee@gmail.com>
2024-04-09 16:17:23 +0700

    Refactor RootPane class and add missing newline at end of file

--- src/Pane/RootPane.java
+++ src/Pane/RootPane.java
@@ -1,2 +1,30 @@
-package Pane;public class RootPane {
+package pane;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import main.GamePanel;
+import utils.Goto;
+
+public class RootPane extends StackPane {
+    private static RootPane instance;
+
+    public RootPane() {
+        this.setWidth(GamePanel.getInstance().getScreenWidth());
+        this.setHeight(GamePanel.getInstance().getScreenHeight());
+        Goto.setPane(this);
+        Goto.titleScreen();
+
+    }
+
+    public static RootPane getRootPane() {
+        if (instance == null)
+            instance = new RootPane();
+        return instance;
+    }
 }
