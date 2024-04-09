a1d7f8c19f7b5a8b62207f58c283e4f5187b2723
Siratee Topradit <tae.siratee@gmail.com>
2024-04-09 16:16:41 +0700

    Add setMonster method to AssetSetter class

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,2 +1,22 @@
-package main;public class AssetSetter {
+package main;
+
+import entity.Entity;
+import entity.Zomby;
+
+public class AssetSetter {
+    GamePanel gp;
+    public AssetSetter(GamePanel gp) {
+        this.gp = gp;
+    }
+    public void setMonster() {
+        Entity[] monster = gp.getMonster();
+        for (int i = 0; i < 20; i++) {
+            monster[i] = new Zomby(gp);
+            int randomX = (int) (Math.random() * gp.worldScreenWidth);
+            int randomY = (int) (Math.random() * gp.worldScreenHeight);
+            monster[i].setWorldX(randomX);
+            monster[i].setWorldY(randomY);
+        }
+        gp.setMonster(monster);
+    }
 }
