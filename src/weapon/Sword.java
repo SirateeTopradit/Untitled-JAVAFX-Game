9e1a1d1dd7a0e35b928f5921a33669617009c526
Siratee Topradit <tae.siratee@gmail.com>
2024-04-27 14:13:00 +0700

    Refactor Sword class: Add constructor and set initial properties

--- src/weapon/Sword.java
+++ src/weapon/Sword.java
@@ -1,2 +1,15 @@
-package weapon;public class Sword {
+package weapon;
+import javafx.scene.shape.Rectangle;
+import main.GamePanel;
+
+public class Sword extends Weapon{
+    public Sword(GamePanel gp) {
+        super(gp);
+        System.out.println("Sword created");
+        setHitBox(new Rectangle(0, 5*20, 2*20, 4*20));
+        setScreenX((gp.getScreenWidth()/2)-60-40);
+        setScreenY((gp.getScreenHeight()/2)-110-40);
+        setWorldX((gp.getWorldScreenWidth() / 2)-60-40);
+        setWorldY((gp.getWorldScreenHeight() / 2)-100-40);
+    }
 }
