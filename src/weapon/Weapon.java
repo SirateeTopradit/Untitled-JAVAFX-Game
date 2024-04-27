509b19d6c1f10b6f94b561036e6ef5efecce93b2
Siratee Topradit <tae.siratee@gmail.com>
2024-04-27 14:13:04 +0700

    Refactor Weapon class: Add weapon drawing logic and properties

--- src/weapon/Weapon.java
+++ src/weapon/Weapon.java
@@ -1,2 +1,70 @@
-package weapon;public class weapon {
+package weapon;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import main.GamePanel;
+
+public class Weapon {
+    GamePanel gp;
+    private Rectangle hitBox;
+    private int screenX;
+    private int screenY;
+    private int worldX;
+    private int worldY;
+    public Weapon(GamePanel gp) {
+        this.gp = gp;
+    }
+    public void draw(GraphicsContext gc) {
+        gc.setFill(javafx.scene.paint.Color.BLUE);
+        gc.fillRect(screenX + getHitBox().getX(), screenY + getHitBox().getY(), getHitBox().getWidth(), getHitBox().getHeight());
+    }
+
+    public GamePanel getGp() {
+        return gp;
+    }
+
+    public void setGp(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public Rectangle getHitBox() {
+        return hitBox;
+    }
+
+    public void setHitBox(Rectangle hitBox) {
+        this.hitBox = hitBox;
+    }
+
+    public int getScreenX() {
+        return screenX;
+    }
+
+    public void setScreenX(int screenX) {
+        this.screenX = screenX;
+    }
+
+    public int getScreenY() {
+        return screenY;
+    }
+
+    public void setScreenY(int screenY) {
+        this.screenY = screenY;
+    }
+
+    public int getWorldX() {
+        return worldX;
+    }
+
+    public void setWorldX(int worldX) {
+        this.worldX = worldX;
+    }
+
+    public int getWorldY() {
+        return worldY;
+    }
+
+    public void setWorldY(int worldY) {
+        this.worldY = worldY;
+    }
 }
