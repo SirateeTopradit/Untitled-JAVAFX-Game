7ce0a5057231106672dd1a58567cadff25132578
Siratee Topradit <tae.siratee@gmail.com>
2024-04-27 12:52:02 +0700

    Refactor AssetSetter class: Update setMonster method to limit the number of monsters and adjust their positions

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -10,12 +10,12 @@ public class AssetSetter {
     }
     public void setMonster() {
         Entity[] monster = gp.getMonster();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 8; i++) {
             monster[i] = new Zomby(gp);
-            int randomX = (int) (Math.random() * gp.worldScreenWidth);
-            int randomY = (int) (Math.random() * gp.worldScreenHeight);
-            monster[i].setWorldX(randomX);
-            monster[i].setWorldY(randomY);
+            int randomX = (int) (Math.random() * gp.worldScreenWidth-1080);
+            int randomY = (int) (Math.random() * gp.worldScreenHeight-1920);
+            monster[i].setWorldX(randomX+1080);
+            monster[i].setWorldY(randomY+1920);
         }
         gp.setMonster(monster);
     }
