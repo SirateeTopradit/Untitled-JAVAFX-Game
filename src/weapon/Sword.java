bc9c4343d1e2479ad0a9400bfbea48b682c629c2
Siratee Topradit <tae.siratee@gmail.com>
2024-04-28 22:23:00 +0700

    Refactor Sword class to improve hit box and timer functionality

--- src/weapon/Sword.java
+++ src/weapon/Sword.java
@@ -5,21 +5,20 @@ import javafx.scene.shape.Rectangle;
 import main.GamePanel;
 
 import java.io.InputStream;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Sword extends Weapon{
-    private final long startTime;
     private Image[] frames = new Image[getNUM_FRAMES()];
     public Sword(GamePanel gp) {
         super(gp);
-        System.out.println("Sword created");
         setHitBox(new Rectangle(0, 0, 200, 200));
         setScreenX((gp.getScreenWidth()/2)-60-40);
         setScreenY((gp.getScreenHeight()/2)-110);
         setAvailable(false);
         startTimer();
-        startTime = System.currentTimeMillis();
+        setStartTime(System.currentTimeMillis());
         try {
             for (int i = 0; i < getNUM_FRAMES(); i++) {
                 InputStream resource = getClass().getClassLoader().getResourceAsStream(String.format("hit/hit%02d.png", i));
@@ -33,7 +32,7 @@ public class Sword extends Weapon{
         }
     }
     public Image getCurrentFrame() {
-        int index = (int) ((System.currentTimeMillis() - startTime) / 50) % getNUM_FRAMES();
+        int index = (int) ((System.currentTimeMillis() - getStartTime()) / 50) % getNUM_FRAMES();
         return frames[index];
     }
     @Override
@@ -41,7 +40,7 @@ public class Sword extends Weapon{
         setWorldX(gp.getPlayer().getWorldX());
         setWorldY(gp.getPlayer().getWorldY());
         if (isAvailable()) {
-            setHitBox(new Rectangle(0, 0, 200, 200));
+            setHitBox(new Rectangle(0, 0, 400, 400));
         }else {
             setHitBox(new Rectangle(0, 0, 0, 0));
         }
@@ -49,10 +48,20 @@ public class Sword extends Weapon{
     @Override
     public void draw(GraphicsContext gc) {
         if (isAvailable()) {
-            gc.drawImage(getCurrentFrame(), getScreenX(), getScreenY(),200,200);
+            gc.drawImage(getCurrentFrame(), getScreenX()-100, getScreenY()-100,400,400);
 //            gc.setFill(javafx.scene.paint.Color.BLUE);
 //            gc.fillRect(screenX + getHitBox().getX(), screenY + getHitBox().getY(), getHitBox().getWidth(), getHitBox().getHeight());
         }
+    }
+    public void startTimer() {
+        setTimer(new Timer());
+        getTimer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Timer task started");
+                setAvailable(!isAvailable());
+            }
+        }, 0, 2000);
     }
 
 
