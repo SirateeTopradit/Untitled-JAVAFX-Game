462b3be88fb410c8e947715ad2d27e7b829f63fc
Siratee Topradit <tae.siratee@gmail.com>
2024-04-28 22:21:58 +0700

    Refactor Bone class to remove unused code and optimize resource loading

--- src/entity/Bone.java
+++ src/entity/Bone.java
@@ -15,10 +15,8 @@ public class Bone extends Entity {
         setAtk(round(atk * 20));
         setHp(round(hp * 600));
         setPoints(round(hp + spd + atk));
-        setHitBox(new Rectangle(4*20, 5*20, 2*20, 4*20));
-        setHitBoxWalk(new Rectangle(3*20, 5*20, 4*20, 4*20));
         try {
-            for (int j = 0; j < 1; j++) {d
+            for (int j = 0; j < 1; j++) {
                 int direction = j;
                 for (int i = 0; i < getNUM_FRAMES(); i++) {
                     InputStream resource = getClass().getClassLoader().getResourceAsStream(String.format("Bone/Bone_walk%02d.png", i));
